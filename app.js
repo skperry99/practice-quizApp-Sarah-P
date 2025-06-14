7050d84e9f8a74d0f1faeb6272898d4cc2d8f4f6
Sarah <skat1403@gmail.com>
2025-06-14 13:01:05 -0500

    added button disable/enable

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import { quizArray } from "./quiz.js";
 import { rightResponse, wrongResponse } from "./feedback.js";
-import { shuffleArray, getRandom, getRandomArray, disableButtonsInDiv, enableButtonsInDiv } from "./helpers.js";
+import {
+  shuffleArray,
+  getRandom,
+  getRandomArray,
+  disableButtonsInDiv,
+  enableButtonsInDiv,
+} from "./helpers.js";
 
 // TODO - add option to choose number of quiz questions
 // add "Retake Quiz" button
-// add question numbers
 // add reset function to reset when "Next Question button is pressed"
 // require an answer for each question
 
@@ -15,8 +20,10 @@ const labelC = document.getElementById("label-c");
 const labelD = document.getElementById("label-d");
 const feedback = document.getElementById("feedback");
 const nxtQ = document.getElementById("nxtQ");
-const multipleChoiceContainer = document.getElementById("multiple-choice-container");
-const buttons = multipleChoiceContainer.querySelectorAll("button");
+const multipleChoiceContainer = document.getElementById(
+  "multiple-choice-container"
+);
+const choiceButtons = multipleChoiceContainer.querySelectorAll("button");
 
 let pickQuizLength = getRandomArray(quizArray, 5);
 let shuffledArray = shuffleArray(pickQuizLength);
@@ -24,65 +31,54 @@ let shuffledArray = shuffleArray(pickQuizLength);
 let i = 0;
 let score = 0;
 
-askQuestion.innerText = shuffledArray[i].question;
+askQuestion.innerText = `${i + 1}.)    ${shuffledArray[i].question}`;
 labelA.innerText = shuffledArray[i].arrayOfAnswers[0];
 labelB.innerText = shuffledArray[i].arrayOfAnswers[1];
 labelC.innerText = shuffledArray[i].arrayOfAnswers[2];
 labelD.innerText = shuffledArray[i].arrayOfAnswers[3];
 
+// Event listener for multiple choice answer selection
 multipleChoiceContainer.addEventListener("click", function (event) {
-  const selectedAnswer = event.target;
-//   disableButtonsInDiv(multipleChoiceContainer);
+  // Disable multiple choice buttons that weren't selected
+  if (event.target.matches("button")) {
+    const selectedAnswer = event.target;
+    choiceButtons.forEach((button) => {
+      if (button !== selectedAnswer) {
+        button.classList.add("disabled");
+      }
+    });
 
-//   choiceButtons.forEach(button => {
-//     if (button !== event.target) {
-//       choiceButtons.disabled = true;
-//     }
-//   });
-    // buttons.forEach(function(button) {
-    //   if (button !== event.target) {
-    //     button.classList.add("disabled");
-    //   }
-    // })
-
-
+  // Check to see if the answer is correct and output correct/incorrect feedback
   if (selectedAnswer.id == shuffledArray[i].answerIdx) {
-
     feedback.innerText = getRandom(rightResponse);
     feedback.style.visibility = "visible";
     score++;
-
   } else {
-
-    feedback.innerText = `${getRandom(wrongResponse)}\nThe correct answer is: ${shuffledArray[i].correctAnswer}`;
+    feedback.innerText = `${getRandom(wrongResponse)}\nThe correct answer is: ${
+      shuffledArray[i].correctAnswer
+    }`;
     feedback.style.visibility = "visible";
-
   }
+}
 });
 
 nxtQ.addEventListener("click", function () {
-
   i++;
 
   // Check to see if all questions have been answered
   if (i >= shuffledArray.length) {
-
     feedback.innerText = `You have reached the end. \nYou got ${score}/${shuffledArray.length} answers correct`;
     feedback.style.visibility = "visible";
 
     // window.location.href = "./complete.html";
-
   } else {
 
-    // buttons.forEach(function(button) {
-   
-    //     button.classList.remove("disabled");
-    //     choiceButtons.style.background = "green";
-
-    // });
+    choiceButtons.forEach((button) => {
+      button.classList.remove("disabled");
+    });
 
     feedback.style.visibility = "hidden";
-    askQuestion.innerText = shuffledArray[i].question;
+    askQuestion.innerText = `${i + 1}.)    ${shuffledArray[i].question}`;
     labelA.innerText = shuffledArray[i].arrayOfAnswers[0];
     labelB.innerText = shuffledArray[i].arrayOfAnswers[1];
     labelC.innerText = shuffledArray[i].arrayOfAnswers[2];
